6f09eed04ac3dfd3106003445fbdccab57599922
TARUN1528 <k.tarunsaiteja1@gmail.com>
2024-06-18 17:23:05 +0530

    Tarun Changes

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import Header from "../Components/layout/Header";
+import img from "../Assets/image.png";
+import "../Styles/About.css";
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+    <Header />
+    <div className='about_background'>
+      <div>
+    <p className='aboutUs_person_1'></p>
+    <p className='aboutUs_person_1_name'> Name </p>
+    </div>
+    <div>
+    <p className='aboutUs_person_2'></p>
+    <p className='aboutUs_person_2_name'> Name </p>
+    </div>
+    <div>
+    <p className='aboutUs_person_3'></p>
+    <p className='aboutUs_person_3_name'> Name </p>
+    </div>
+    <div>
+    <p className='aboutUs_person_4'></p>
+    <p className='aboutUs_person_4_name'> Name </p>
+    </div>
+    <div>
+    <p className='aboutUs_person_5'></p>
+    <p className='aboutUs_person_5_name'> Name </p>
+    </div>
+    </div>
+    </div>
   )
 }
 
