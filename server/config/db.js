98731a574ef816806b04e8f41214bf5a560e9d3e
surendranaik123 <131369740+surendranaik123@users.noreply.github.com>
2024-06-18 10:59:51 +0530

    change

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectMongos = async () => {
+  try {
+    const res = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Connection successfully');
+  } catch (error) {
+    console.log('Check:', error);
+  }
+};
+
+export default connectMongos;
---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import cors from "cors";
+import connectToMongo from "./config/db.js";
+import userRoutes from "./routes/user.js";
+import  'dotenv/config';
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Connect to MongoDB
+connectToMongo();
+
+const jwtSecret = process.env.SECRET;
+
+
+// Apply middleware
+// app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.json());
+// app.use(cors());
+
+// Apply middleware
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// Routes
+app.use("/api/v1", userRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`API is running on http://localhost:${PORT}`);
+  console.log(jwtSecret); 
+});
+
